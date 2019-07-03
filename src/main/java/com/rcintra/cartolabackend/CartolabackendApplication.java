e950462cd80e812b71f44d15bca23c2a53bd15a5
Rafael Cintra <ra.cintra@gmail.com>
2019-07-03 11:01:59 -0300

    add db

--- src/main/java/com/rcintra/cartolabackend/CartolabackendApplication.java
+++ src/main/java/com/rcintra/cartolabackend/CartolabackendApplication.java
@@ -3,6 +3,7 @@ package com.rcintra.cartolabackend;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.rcintra.cartolabackend.domain.Grupo;
 import com.rcintra.cartolabackend.repository.GrupoRepository;
@@ -14,6 +15,7 @@ public class CartolabackendApplication {
 		SpringApplication.run(CartolabackendApplication.class, args);
 	}
 	
+	@Bean
 	ApplicationRunner applicationRunner(GrupoRepository grupoRepository) {
 		return args -> {
 			grupoRepository.save(new Grupo("Grupo 1"));
