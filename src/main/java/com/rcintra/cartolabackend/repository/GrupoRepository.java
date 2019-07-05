fa75b4f69080c6d9da3a7794d9d35498fbefdfc1
Rafael Cintra <ra.cintra@gmail.com>
2019-07-05 06:52:43 -0300

    alterado para jpa

--- src/main/java/com/rcintra/cartolabackend/repository/GrupoRepository.java
+++ src/main/java/com/rcintra/cartolabackend/repository/GrupoRepository.java
@@ -1,16 +1,10 @@
 package com.rcintra.cartolabackend.repository;
 
-import java.util.Collection;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rcintra.cartolabackend.domain.Grupo;
 
-public interface GrupoRepository extends Repository<Grupo, Integer> {
-
+public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
 	
-	@Transactional(readOnly = true)
-	Collection<Grupo> findAll() throws DataAccessException;
+	
 }
