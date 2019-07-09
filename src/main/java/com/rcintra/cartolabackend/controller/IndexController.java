cf7cdaf42fe83283c19bfb1f21c399f87710319d
Rafael Cintra <ra.cintra@gmail.com>
2019-07-09 15:06:17 -0300

    Incluido thymeleaf

--- src/main/java/com/rcintra/cartolabackend/controller/IndexController.java
+++ src/main/java/com/rcintra/cartolabackend/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.rcintra.cartolabackend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 	
+	@Value("${spring.application.name}")
+	String appName;
+	
 	@GetMapping("/")
-	String hello() {
-		return "Hello Cartola BackEnd";
+	public String homePage(Model model) {
+		model.addAttribute("appName", appName);
+		return "home";
 	}
 	
 }
