653932055c58fbac2237fd5a34917f1db0c42163
Rafael Cintra <ra.cintra@gmail.com>
2019-07-09 14:22:16 -0300

    Campeonato

---
+++ src/main/java/com/rcintra/cartolabackend/model/Campeonato.java
@@ -0,0 +1,45 @@
+package com.rcintra.cartolabackend.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="grupo")
+public class Campeonato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank 
+	@Column(name = "nome")
+	@Size(max=100)
+	private String nome;
+	
+	@ManyToMany(cascade= {CascadeType.ALL})
+	@JoinTable(name="campeonato_grupo", 
+		joinColumns= @JoinColumn(name="id_campeonato"),
+		inverseJoinColumns = @JoinColumn(name="id_grupo")
+	)
+	private List<Grupo> grupo;
+
+}
---
+++ src/main/resources/db/migration/V2.0__init_campeonato.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS campeonato (
+	id SERIAL PRIMARY KEY,
+	nome varchar(100) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS campeonato_grupo (
+	id_campeonato int REFERENCES campeonato (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	id_grupo int REFERENCES grupo (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT campeonato_grupo_pkey PRIMARY KEY (id_campeonato, id_grupo)
+);
