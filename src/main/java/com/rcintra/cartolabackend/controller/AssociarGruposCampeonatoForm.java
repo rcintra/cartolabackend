047fd8a8393d6e8c4285cd465f725cc0c93e412b
Rafael Cintra <ra.cintra@gmail.com>
2019-07-10 23:00:11 -0300

    Associar Grupos ao Campeonato Form

---
+++ src/main/java/com/rcintra/cartolabackend/controller/AssociarGruposCampeonatoForm.java
@@ -0,0 +1,12 @@
+package com.rcintra.cartolabackend.controller;
+
+import lombok.Data;
+
+@Data
+public class AssociarGruposCampeonatoForm {
+	
+	private Integer campeonatoId;
+	
+	
+
+}
--- src/main/java/com/rcintra/cartolabackend/controller/CampeonatoGrupoController.java
+++ src/main/java/com/rcintra/cartolabackend/controller/CampeonatoGrupoController.java
@@ -1,16 +1,31 @@
 package com.rcintra.cartolabackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rcintra.cartolabackend.repository.CampeonatoRepository;
+import com.rcintra.cartolabackend.repository.GrupoRepository;
+
 @Controller
 @RequestMapping("/campeonato/grupo/")
 public class CampeonatoGrupoController {
+	
+	@Autowired
+	private GrupoRepository grupoRepository;
+	
+	@Autowired
+	private CampeonatoRepository campeonatoRepository;
 
 	@GetMapping("/home")
 	public String index(Model model) {
+		AssociarGruposCampeonatoForm form = new AssociarGruposCampeonatoForm();
+		
+		model.addAttribute("form", form);
+		model.addAttribute("grupos", grupoRepository.findAll());
+		model.addAttribute("campeonatos", campeonatoRepository.findAll());
 		return "campeonato-grupo";
 	}
 }
